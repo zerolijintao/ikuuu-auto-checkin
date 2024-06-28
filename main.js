ae4800881590ed3089cf8ac3689fdc693ce2d186
ewigl <ewiglichtl@gmail.com>
2024-06-28 23:03:54 +0800

    log in to check in

--- main.js
+++ main.js
@@ -1,10 +1,64 @@
-const checkInUrl = "https://ikuuu.pw/user/checkin";
+const host = "ikuuu.pw";
+const logInUrl = `https://${host}/auth/login`;
+const checkInUrl = `https://${host}/user/checkin`;
+
+function parseCookie(rawCookie) {
+  let cookieSets = rawCookie.split("path=/,");
+
+  // 用于存储去重后的Cookie键值对
+  const cookies = {};
+
+  // 遍历 cookieSets 数组
+  cookieSets.forEach((cookie) => {
+    // 利用正则表达式提取字段名和字段值
+    const match = cookie.match(/^([^=]+)=(.*?);/);
+    if (match) {
+      const fieldName = match[1].trim();
+      let fieldValue = match[2].trim();
+
+      // 对字段值进行解码
+      fieldValue = decodeURIComponent(fieldValue);
+
+      // 存储到cookies对象中（确保每个字段只有一个值，即去重）
+      if (!cookies[fieldName]) {
+        cookies[fieldName] = fieldValue;
+      }
+    }
+  });
+
+  return cookies;
+}
+
+function generateCookieStr(cookieObject) {
+  // 将对象转换为Cookie格式的字符串
+  return Object.entries(cookieObject)
+    .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+    .join("; ");
+}
+
+async function logIn(email, passwd) {
+  let formData = new FormData();
+  formData.append("host", host);
+  formData.append("email", email);
+  formData.append("passwd", passwd);
+  formData.append("code", "");
+  formData.append("remember_me", "off");
+
+  let response = await fetch(logInUrl, {
+    method: "POST",
+    body: formData,
+  });
+
+  let rawCookie = response.headers.get("set-cookie");
+
+  return parseCookie(rawCookie);
+}
 
 function checkIn(cookie) {
   fetch(checkInUrl, {
     method: "POST",
     headers: {
-      Cookie: cookie,
+      Cookie: generateCookieStr(cookie),
     },
   })
     .then((res) => res.json())
@@ -16,14 +70,18 @@ function checkIn(cookie) {
 }
 
 async function main() {
-  let cookie;
+  let email;
+  let passwd;
 
-  if (process.env.COOKIE) {
-    cookie = process.env.COOKIE;
+  if (process.env.EMAIL && process.env.PASSWD) {
+    email = process.env.EMAIL;
+    passwd = process.env.PASSWD;
   } else {
-    console.log("COOKIE NOT FOUND");
+    console.log("ENV ERROR");
     process.exit(1);
   }
+
+  let cookie = await logIn(email, passwd);
 
   checkIn(cookie);
 }
